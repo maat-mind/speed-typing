f3f580c42da8a29c7196fa21150b2ad306e8a698
Oscar Bernal <odbg2013@gmail.com>
2022-07-08 18:30:56 -0500

    speed typing completed

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
 
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span');
   const arrayValue = quoteInputElement.value.split('');
+  let correct = true;
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index];
     if (character == null) {
       characterSpan.classList.remove('correct');
       characterSpan.classList.remove('incorrect');
+      correct = false;
     } else if (character == characterSpan.innerText) {
       characterSpan.classList.add('correct');
       characterSpan.classList.remove('incorrect');
-    } else if (character != characterSpan.innerText) {
+    } else {
       characterSpan.classList.add('incorrect');
       characterSpan.classList.remove('correct');
+      correct = false;
     }
   });
+  if (correct) renderNewQuote();
 });
 
 function getRandomQuote() {
@@ -35,6 +40,22 @@ async function renderNewQuote() {
     quoteDisplayElement.appendChild(characterSpan);
   });
   quoteInputElement.value = null;
+  startTimer();
+}
+
+let startTime;
+
+function startTimer() {
+  startTime = new Date();
+
+  timerElement.innerText = 0;
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+ return Math.floor((new Date() - startTime) / 1000)
 }
 
 renderNewQuote();
